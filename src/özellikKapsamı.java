0eec50facf74b865b7a1ff47322d0ca874678455
Ahmet Selim Fedakar <selimfedakar@Ahmet-MacBook-Pro-2.local>
2023-03-30 00:40:55 +0300

    emty

---
+++ src/özellikKapsamı.java
@@ -0,0 +1,13 @@
+
+public class özellikKapsamı {
+    
+    /*  --- SCOPE 
+    class ımızın bir özelliği olabilir  for ex. x , aynı zamanda bir metod tanımladık oradaki integer değer de x ise ;;
+    biz local yani içerdeki private değerimiz bastırılır.
+    AYNI İSME SAHİP DEĞİŞKENLER MEYDANA GELDİĞİNDE,ÜRETTİĞİMİZDE ,
+    EN LOCAL EN YEREL OLAN KULLANILIR.
+    */
+    
+    
+    
+}
